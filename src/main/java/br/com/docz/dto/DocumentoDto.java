40e2dfee5fca63bfd4034a1a6b8e1ef4c901fe17
DANIEL CHIPOLESCH <danielchipolesch@gmail.com>
2023-08-28 21:56:07 -0300

    Many attributes have been added to a DocumentoDto for allowing transfer data from/to Documento object from other objects.

--- src/main/java/br/com/docz/dto/DocumentoDto.java
+++ src/main/java/br/com/docz/dto/DocumentoDto.java
@@ -1,9 +1,18 @@
 package br.com.docz.dto;
 
 
+import br.com.docz.model.entity.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
-public record DocumentoDto(String textoDocumento) {
+public record DocumentoDto(@NotBlank String nomeOrgao,
+                           Especie especie,
+                           AssuntoBasico assuntoBasico,
+                           @NotBlank String nomeEpigrafeDocumento,
+                           @NotNull Integer numeroSecundarioAssuntoBasico,
+                           AtoAprovacao atoAprovacao,
+                           Sumario sumario,
+                           Prefacio prefacio,
+                           Referencia referencia) {
 
 }
