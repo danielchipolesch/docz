aaf01d71413768d7cb45e23a55d12461ec81196f
DANIEL CHIPOLESCH <danielchipolesch@gmail.com>
2023-08-28 21:58:24 -0300

    Lombok anotations @Getter and @Setter have been successfully included. It's not necessary include getter and setters manually anymore. Also was included names for constraints by using @JoinColumn(foreignKey = @ForeignKey(name = 'fk_name')) annotation for each attribute.

--- src/main/java/br/com/docz/model/entity/Documento.java
+++ src/main/java/br/com/docz/model/entity/Documento.java
@@ -1,11 +1,15 @@
 package br.com.docz.model.entity;
-import com.fasterxml.jackson.annotation.JsonBackReference;
+import br.com.docz.helper.StatusDocumento;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.annotations.Cascade;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.DynamicUpdate;
 import org.hibernate.annotations.UpdateTimestamp;
-import org.springframework.data.domain.Sort;
 
 import java.sql.Timestamp;
 import java.util.*;
@@ -13,6 +17,10 @@ import java.util.*;
 @Entity
 @Table(name = "t_documento")
 @DynamicUpdate
+@Getter
+@Setter
+@EqualsAndHashCode
+@ToString
 public class Documento {
 	
 	@Id
@@ -20,18 +28,50 @@ public class Documento {
 	@Column(name = "cd_documento")
 	private Integer codigoDocumento;
 	
-	@Column(name = "tx_documento")
-	private String textoDocumento;
+	@Column(name = "nm_orgao")
+	private String nomeOrgao;
+	
+	@OneToOne
+	@JoinColumn(foreignKey = @ForeignKey(name = "fk_especie"))
+	private Especie especie;
+	
+	@OneToOne
+	@JoinColumn(foreignKey = @ForeignKey(name = "fk_assunto_basico"))
+	private AssuntoBasico assuntoBasico;
+	
+	@Column(name = "nm_epigrafe_documento")
+	private String nomeEpigrafeDocumento;
+	
+	@Column(name = "nr_secundario_assunto_basico")
+	private Integer numeroSecundarioAssuntoBasico;
+	
+	@OneToOne
+	@JoinColumn(foreignKey = @ForeignKey(name = "fk_ato_aprovacao"))
+	private AtoAprovacao atoAprovacao;
+	
+	@OneToOne
+	@JoinColumn(foreignKey = @ForeignKey(name = "fk_sumario"))
+	private Sumario sumario;
+	
+	@OneToOne
+	@JoinColumn(foreignKey = @ForeignKey(name = "fk_prefacio"))
+	private Prefacio prefacio;
 	
 	@OneToMany (mappedBy = "documento", cascade = {CascadeType.REMOVE, CascadeType.PERSIST})
 	@JsonManagedReference
 	@OrderBy("nr_ordem_capitulo ASC")
 	private List<Capitulo> capitulos = new ArrayList<>();
 	
+	@OneToOne
+	@JoinColumn(foreignKey = @ForeignKey(name = "fk_referencia"))
+	private Referencia referencia;
+	
 	@OneToMany (mappedBy = "documento", cascade = {CascadeType.REMOVE, CascadeType.PERSIST})
 	@JsonManagedReference
-	//@OrderBy("nr_ordem_capitulo ASC")
 	private List<Anexo> anexos = new ArrayList<>();
+	
+	@Enumerated(EnumType.STRING)
+	private StatusDocumento statusDocumento;
 	
 	@Column(name = "dt_criacao", updatable = false)
 	@CreationTimestamp
@@ -44,52 +84,4 @@ public class Documento {
 	@Column(name = "nr_versao")
 	@Version
 	private Integer numeroVersao;
-	
-	public Integer getCodigoDocumento() {
-		return codigoDocumento;
-	}
-	
-	public void setCodigoDocumento(Integer codigoDocumento) {
-		this.codigoDocumento = codigoDocumento;
-	}
-	
-	public String getTextoDocumento() {
-		return textoDocumento;
-	}
-	
-	public void setTextoDocumento(String textoDocumento) {
-		this.textoDocumento = textoDocumento;
-	}
-	
-	public List<Capitulo> getCapitulos() {
-		return capitulos;
-	}
-
-	public void setCapitulos(List<Capitulo> capitulos) {
-		this.capitulos = capitulos;
-	}
-	
-	public Timestamp getDataCriacao() {
-		return dataCriacao;
-	}
-	
-	public void setDataCriacao(Timestamp dataCriacao) {
-		this.dataCriacao = dataCriacao;
-	}
-	
-	public Timestamp getDataAlteracao() {
-		return dataAlteracao;
-	}
-	
-	public void setDataAlteracao(Timestamp dataAlteracao) {
-		this.dataAlteracao = dataAlteracao;
-	}
-	
-	public Integer getNumeroVersao() {
-		return numeroVersao;
-	}
-	
-	public void setNumeroVersao(Integer numeroVersao) {
-		this.numeroVersao = numeroVersao;
-	}
 }
